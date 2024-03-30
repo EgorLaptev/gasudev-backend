f57c96b13870ace6d40202c6d4bcaa2db4569e40
EgorLaptev <egor.lapteffff@gmail.com>
2024-03-31 01:56:37 +0300

    update

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,6 +15,7 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'username' => $this->username,
             'name' => $this->name,
             'surname' => $this->surname,
